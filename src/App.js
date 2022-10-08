cc54773e1da0bfb51e3a5d476907575990d3b72d
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2022-10-09 01:58:36 +0200

    structed and created components folder to jsx components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
 const App = () => (
     <BrowserRouter>
-     <Box sx={{ backgroundColor: '#000' }}>
+     <Box sx={{ backgroundColor: '#000'}}>
       <Navbar />
         <Routes>
             <Route path='/' exact element={<Feed />} />
