7b7a02689689b46ed8482f9648e9e39de9ac3ef2
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2022-10-10 02:17:59 +0200

    created new component Sidebar.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './SideBar';
 
 const Feed = () => {
   return (
@@ -8,7 +9,7 @@ const Feed = () => {
     <Box sx={{ height: { sx: 'auto', md:
     '92vh' }, borderRight: '1px solid #3d3d3d',
     px: { sx: 0, md: 2 }}}>
-    Sidebar 
+    <Sidebar /> 
 
     <Typography className="copyright"
     variant="body2" sx={{ mt: 1.5,
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { Stack } from '@mui/icons-material';
+
+import { categories } from '../utils/constants';
+
+const Sidebar = () => (
+    <Stack>
+        
+    </Stack>
+)
+
+export default Sidebar
