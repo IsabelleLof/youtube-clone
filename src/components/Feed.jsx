b324c93e1471a9e2c8de82a51f177425b5a1bcaf
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2022-10-12 22:28:08 +0200

    style is done for the Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -17,12 +17,14 @@ const Feed = () => {
       Copyright 2022 Bell Media
     </Typography>
     </Box>
-    <Box>
+    <Box p={2} sx={{ overflowY: 'auto',
+    height: '90vh', flex: 2}} >
       <Typography variant="h4"
       fontWeight="bold" mb={2} sx={{
         color: 'white' }}>
        New <span style={{ color: '#F31503' }}>videos</span>
       </Typography>
+      <Videos videos={[]} />
     </Box>
     </Stack>
   )
