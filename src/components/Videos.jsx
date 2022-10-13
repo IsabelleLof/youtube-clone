6fd4a6b3412fc93220b8bb8c22ae783b0fc5a50b
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2022-10-13 21:44:05 +0200

    useEffect and states in Feed.jsx connected to API with a BASE url using axios

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Videos = () => {
+import { Stack, Box } from "@mui/material";
+
+const Videos = ({ videos }) => {
+    //console.log(videos);
   return (
-    <div>Videos</div>
+      <Stack direction="row" flexWrap="wrap"
+      justifyContent="start" gap={2}>
+      
+      </Stack>
   )
 }
 
-export default Videos
+export default Videos;
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search'
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
     //url: BASE_URL,
@@ -8,10 +8,9 @@ const options = {
       maxResults: '50'
     },
     headers: {
-      'X-RapidAPI-Key': process.env.
-      REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    },
   };
 
   ///baseUrl/getVideos
